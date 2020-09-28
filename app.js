eaac042308f65adaaff2fcb58739d5c5e7198d5d
Roland O. Aifuwa <siroapple10@gmail.com>
2020-09-27 19:26:00 -0500

    added api to query

--- app.js
+++ app.js
@@ -11,35 +11,36 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function (req, res) {
 res.sendFile(__dirname+ "/index.html");
 
-    // const location = "Lagos";
-    // const apiKey = "0a4b2a6c74e97a23633edd8d5b095612";
-    // const metric="metric";
-    // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey+"&units="+metric;
-    // https.get(url, function (response) {
-    //     console.log(response.statusCode); //get statuscode
-
-    //     response.on("data", function (data) { //read data from api
-    //         // console.log(data); //gives data in hex
-    //         const weatherData = JSON.parse(data); //converts data to JSON
-    //         //console.log(weatherData);
-    //         const temp = weatherData.main.temp;
-    //         const weatherDes = weatherData.weather[0].description;
-    //         const icon = weatherData.weather[0].icon;
-    //         const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    //         console.log(weatherDes);
-    //         res.write("<p>The weather is currently " + weatherDes + "</p>");
-    //         res.write("<h1>The temperature in " + location + " is " + temp + " degree.</h1>");
-    //         res.write("<img src=" + imageURL + ">");
-    //         res.send();
-    //     });
-    // });
-
-
 });
 
 app.post("/",function(req, res){
-    console.log(req.body.cityName);
     console.log("Post received");
+
+    
+    const location = req.body.cityName;
+    const apiKey = "0a4b2a6c74e97a23633edd8d5b095612";
+    const metric="metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey+"&units="+metric;
+    https.get(url, function (response) {
+        console.log(response.statusCode); //get statuscode
+
+        response.on("data", function (data) { //read data from api
+            // console.log(data); //gives data in hex
+            const weatherData = JSON.parse(data); //converts data to JSON
+            //console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const weatherDes = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+            console.log(weatherDes);
+            res.write("<p>The weather is currently " + weatherDes + "</p>");
+            res.write("<h1>The temperature in " + location + " is " + temp + " degree.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
+
+
     });
 app.listen(3000, function () {
     
